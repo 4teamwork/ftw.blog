b3c7177e600311da42a9d6f702aaf1e2fda86429
Jonas Baumann <j.baumann@4teamwork.ch>
2008-07-17 13:18:11 +0000

    #15 Kategorien-Portlet in Blog

--- izug/blog/__init__.py
+++ izug/blog/__init__.py
@@ -1,3 +1,5 @@
+from zope.i18nmessageid import MessageFactory
+blogMessageFactory = MessageFactory('izug.blog')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
