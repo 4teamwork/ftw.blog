39f3604672bb3a16e2a801795e924e8b4fb535e1
Philippe Gross <p.gross@4teamwork.ch>
2010-08-04 10:27:10 +0200

    modified blog_entry_view

--- ftw/blog/viewlets/viewlets.py
+++ ftw/blog/viewlets/viewlets.py
@@ -1,12 +1,23 @@
 from plone.app.layout.viewlets import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.comments import CommentsViewlet
-
+from ftw.tagging.browser.viewlets.taglist import TagListViewlet
 
 class FtwBlogActionsBar(ViewletBase):
-
+    """ A Viewlet wich show the blog specific searchfield """
     render = ViewPageTemplateFile('ftw_blog_actionsbar.pt')
 
 
 class CommentsViewlet(CommentsViewlet):
     render = ViewPageTemplateFile('ftw_comments.pt')
+
+
+class HiddenTagList(TagListViewlet):
+    """ Hide the Taglist from ftw.tagging """
+
+    def update(self):
+        pass
+        
+    def index(self):
+        return ''
+
