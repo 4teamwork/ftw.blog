c896eaf665b14311e260677b821ecab7de0200a6
Philippe Gross <p.gross@4teamwork.ch>
2010-08-10 10:11:17 +0200

    Issue #306 ftw.blog: Portlets Archiv, Tag Cloud nicht anzeigen, falls Blog leer   https://extranet.4teamwork.ch/intranet/10-interne-projekte/4teamwork-egov/tracker-4teamwork-egov/306

--- ftw/blog/portlets/archiv.py
+++ ftw/blog/portlets/archiv.py
@@ -34,7 +34,6 @@ class Assignment(base.Assignment):
     def title(self):
         return "Blog Archive Portlet"
 
-
 class Renderer(base.Renderer):
 
     def __init__(self, context, request, view, manager, data):
@@ -44,6 +43,23 @@ class Renderer(base.Renderer):
         self._translation_service = getToolByName(
                                         context,
                                         'translation_service')
+
+    @property
+    def available(self):
+        """Only show the portlet, when the blog isn't empty
+        """
+        catalog = getToolByName(self.context, 'portal_catalog')
+        blogutils = getUtility(IBlogUtils, name='ftw.blog.utils')
+        blogroot = blogutils.getBlogRoot(self.context)
+        root_path ='/'.join(blogroot.getPhysicalPath())
+
+        all_entries = catalog({'path': root_path,
+                              'portal_type': 'BlogEntry'})
+        if len(all_entries) > 0:
+            return True
+        else:
+            return False
+
 
     def zLocalizedTime(self, time, long_format=False):
         """Convert time to localized time
