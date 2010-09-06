f617c14332e242d813899955de96723be4292287
Philippe Gross <p.gross@4teamwork.ch>
2010-09-06 17:25:29 +0200

    added all plone functions so that all plone defined overlays also works

--- ftw/blog/skins/ftw.blog_templates/popupforms.js
+++ ftw/blog/skins/ftw.blog_templates/popupforms.js
@@ -10,8 +10,16 @@ var common_jqt_config = {fixed:false,speed:'fast',mask:{color:'#fff',opacity: 0.
 
 jQuery.extend(jQuery.tools.overlay.conf, common_jqt_config);
 
+
 jQuery(function($){
 
+    if (jQuery.browser.msie && parseInt(jQuery.browser.version, 10) < 7) {
+        // it's not realistic to think we can deal with all the bugs
+        // of IE 6 and lower. Fortunately, all this is just progressive
+        // enhancement.
+        return;
+    }
+    
     // method to show error message in a noform
     // situation.
     function noformerrorshow(el, noform) {
@@ -34,6 +42,40 @@ jQuery(function($){
         return location;
     }
 
+    // login form
+    $('#portal-personaltools a[href$=/login], #portal-personaltools a[href$=/login_form], .discussion a[href$=/login_form]').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            formselector: 'form#login_form',
+            noform: function () {
+                if (location.href.search(/pwreset_finish$/) >= 0) {
+                    return 'redirect';
+                } else {
+                    return 'reload';
+                }
+            },
+            redirect: function () {
+                var href = location.href;
+                if (href.search(/pwreset_finish$/) >= 0) {
+                    return href.slice(0, href.length-14) + 'logged_in';
+                } else {
+                    return href;
+                }
+            }
+        }
+    );
+
+    // contact form
+    $('#siteaction-contact a').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            formselector: 'form',
+            noform: function(el) {return noformerrorshow(el, 'redirect');}
+        }
+    );
+
     // comment form
     $('form[name=reply]').prepOverlay(
         {
@@ -44,6 +86,92 @@ jQuery(function($){
             redirect: redirectbasehref
         }
     );
+
+    // display: select content item / change content item
+    $('#contextSetDefaultPage, #folderChangeDefaultPage').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            formselector: 'form[name="default_page_form"]',
+            noform: function(el) {return noformerrorshow(el, 'reload');},
+            closeselector: '[name=form.button.Cancel]',
+            width:'40%'
+        }
+    );
+
+    // advanced state
+    // This form needs additional JS and CSS for the calendar widget.
+    // The AJAX form doesn't load it from the javascript_head_slot.
+    // $('dl#plone-contentmenu-workflow a#advanced').prepOverlay(
+    //     {
+    //         subtype: 'ajax',
+    //         filter: common_content_filter,
+    //         formselector: 'form',
+    //         noform: function(el) {return noformerrorshow(el, 'reload');},
+    //         closeselector: '[name=form.button.Cancel]'
+    //     }
+    // );
+
+    // Delete dialog
+    $('dl#plone-contentmenu-actions a#delete').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            formselector: 'form',
+            noform: function(el) {return noformerrorshow(el, 'redirect');},
+            redirect: redirectbasehref,
+            closeselector: '[name=form.button.Cancel]',
+            width:'50%'
+        }
+    );
+
+    // Rename dialog
+    $('dl#plone-contentmenu-actions a#rename').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            closeselector: '[name=form.button.Cancel]',
+            width:'40%'
+        }
+    );
+
+    // Select default view dialog
+    $('dl#plone-contentmenu-display a#contextSetDefaultPage').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            formselector: 'form',
+            noform: function(el) {return noformerrorshow(el, 'reload');},
+            closeselector: '[name=form.button.Cancel]'
+        }
+    );
+
+    // registration
+    $('#portal-personaltools a[href$=/@@register]').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            formselector: 'form.kssattr-formname-register'
+        }
+    );
+
+    // add new user, group
+    $('form[name=users_add], form[name=groups_add]').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            formselector: 'form.kssattr-formname-new-user, form[name="groups"]',
+            noform: function(el) {return noformerrorshow(el, 'redirect');},
+            redirect: function () {return location.href;}
+        }
+    );
+
+    // Content history popup
+    $('#content-history a').prepOverlay({
+       subtype: 'ajax', 
+       urlmatch: '@@historyview',
+       urlreplace: '@@contenthistorypopup'
+    });
 
 });
 
