8bf8e45116004d669a8ec8970169561852d4d671
Elio Schmutz <e.schmutz@4teamwork.ch>
2011-04-20 14:27:32 +0200

    setup.py: added missing dependencies

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(name='ftw.blog',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+        'Products.LinguaPlone',
           'setuptools',
           'plone.browserlayer',
           'ftw.tagging',
