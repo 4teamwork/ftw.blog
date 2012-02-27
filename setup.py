b8c23de24c720b3e3950796e9543e615817e8f1f
Jonas Baumann <jone@jone.ch>
2012-02-27 14:00:25 +0100

    Cleanup

--- setup.py
+++ setup.py
@@ -14,8 +14,9 @@ tests_require = [
 setup(name='ftw.blog',
       version=version,
       description="A Blog for Plone",
-      long_description=open("README.rst").read() + "\n" +
-        open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -26,30 +27,32 @@ setup(name='ftw.blog',
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
+
       keywords='ftw blog',
       author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
       url='https://github.com/4teamwork/ftw.blog',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.browserlayer',
-          'ftw.tagging',
-          'Plone',
-          # -*- Extra requirements: -*-
-      ],
 
+      install_requires=[
+        'setuptools',
+        'plone.browserlayer',
+        'ftw.tagging',
+        'Plone',
+        # -*- Extra requirements: -*-
+        ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-
       )
