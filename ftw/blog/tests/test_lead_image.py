b26510a8883530a67582510ec4caf06272e6d620
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-10-19 22:05:40 +0200

    Show lead image on blog entry view, as first image in listing

--- ftw/blog/tests/test_lead_image.py
+++ ftw/blog/tests/test_lead_image.py
@@ -4,6 +4,7 @@ from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.registry.interfaces import IRegistry
 from pyquery import PyQuery
+from StringIO import StringIO
 from unittest2 import TestCase
 from zope.component import getUtility
 
@@ -21,6 +22,10 @@ class TestLeadImage(TestCase):
         blog = self.portal.get(self.portal.invokeFactory('Blog', 'blog'))
         self.entry = blog.get(blog.invokeFactory('BlogEntry', 'entry'))
 
+    def enable_lead_image(self):
+        registry = getUtility(IRegistry)
+        registry.forInterface(IBlogSettings).blog_entry_has_lead_image = True
+
     def test_blog_entry_has_no_lead_image_by_default(self):
 
         field = self.entry.Schema()['leadimage']
@@ -28,9 +33,38 @@ class TestLeadImage(TestCase):
             field.widget.testCondition(None, self.portal, self.entry))
 
     def test_blog_entry_has_lead_image(self):
-        registry = getUtility(IRegistry)
-        registry.forInterface(IBlogSettings).blog_entry_has_lead_image = True
+        self.enable_lead_image()
 
         field = self.entry.Schema()['leadimage']
         self.assertTrue(
             field.widget.testCondition(None, self.portal, self.entry))
+
+    def test_do_not_show_image_on_entry_view(self):
+        view = self.entry.restrictedTraverse('@@blog_entry_view')
+
+        self.assertFalse(view.show_lead_image())
+
+        self.enable_lead_image()
+        # Still false, because there is no image
+        self.assertFalse(view.show_lead_image())
+
+        doc = PyQuery(view())
+        self.assertFalse(doc('.leadimage'))
+
+    def test_show_lead_image(self):
+        image = StringIO(
+            'GIF89a\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00\x00\x00'
+            '\x00!\xf9\x04\x04\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00'
+            '\x01\x00\x00\x02\x02D\x01\x00;')
+
+        self.entry.setLeadimage(image)
+
+        view = self.entry.restrictedTraverse('@@blog_entry_view')
+        self.assertFalse(view.show_lead_image())
+
+        self.enable_lead_image()
+        self.assertTrue(view.show_lead_image())
+
+        doc = PyQuery(view())
+        self.assertTrue(doc('.leadimage'))
+
