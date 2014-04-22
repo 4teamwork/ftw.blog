86c37686ed3da40309637d154ec4987a5ca67ba9
Jonas Baumann <jone@jone.ch>
2014-04-22 11:12:34 +0200

    Remove irrelevant conversation test.
    The default conversation behavior of p.a.discussion changed from Plone 4.3.2 to 4.3.3.

--- ftw/blog/tests/test_conversation_patch.py
+++ ftw/blog/tests/test_conversation_patch.py
@@ -1,7 +1,6 @@
 import unittest2 as unittest
 from ftw.blog.testing import FTW_BLOG_INTEGRATION_TESTING
 from zope.interface import alsoProvides
-from plone.app.discussion.interfaces import IDiscussionLayer
 from plone.registry.interfaces import IRegistry
 from zope.component import queryUtility
 from plone.app.discussion.interfaces import IDiscussionSettings
@@ -22,19 +21,7 @@ class TestPatchedConversationView(unittest.TestCase):
 
         self.blog = self.portal.get(self.portal.invokeFactory('Blog', 'blog'))
 
-    def test_enabled_original(self):
-
-        # Provide IDiscussionLayer
-        alsoProvides(
-            self.portal.REQUEST, IDiscussionLayer)
-
-        entry = self.blog.get(self.blog.invokeFactory('BlogEntry', 'entry'))
-        view = entry.restrictedTraverse('@@conversation_view')
-
-        self.assertFalse(view.enabled())
-
     def test_enabled_blog(self):
-
         # Provide IBlogLayer
         alsoProvides(
             self.portal.REQUEST, IBlogLayer)
