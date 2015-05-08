98c5e3fe1e3f043e44bfecddfbd2493ea2b684b6
Jonas Baumann <jone@jone.ch>
2015-05-08 12:09:32 +0200

    4teamwork is an AG.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name='ftw.blog',
         ],
 
       keywords='ftw blog',
-      author='4teamwork GmbH',
+      author='4teamwork AG',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
       url='https://github.com/4teamwork/ftw.blog',
